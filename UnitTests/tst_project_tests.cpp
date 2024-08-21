7722d3af252506c4a5b0d371171fee63ecf902a1
Sergey Bauman <tqqqqt@users.noreply.github.com>
2024-08-21 18:48:32 +0700

    added first test for check QT tests

--- UnitTests/tst_project_tests.cpp
+++ UnitTests/tst_project_tests.cpp
@@ -1,6 +1,7 @@
 #include <QtTest>
 
-// add necessary includes here
+#include "../Calculator/source/calculatormath.h"
+#include "../Calculator/source/numbermath.h"
 
 class project_tests : public QObject
 {
@@ -11,7 +12,8 @@ public:
     ~project_tests();
 
 private slots:
-    void test_case1();
+    void test_polish_entry1();
+    void test_polish_entry2();
 
 };
 
@@ -25,9 +27,15 @@ project_tests::~project_tests()
 
 }
 
-void project_tests::test_case1()
+void project_tests::test_polish_entry1()
 {
+    CalculatorMath* creatorEntry=new CalculatorMath();
+    QCOMPARE(creatorEntry->SetString("2+(2-2+(4*8))"),0);
+}
 
+void project_tests::test_polish_entry2(){
+    CalculatorMath* creatorEntry=new CalculatorMath();
+    QCOMPARE(creatorEntry->SetString("2+(2-2+(4*8)"),2);
 }
 
 QTEST_APPLESS_MAIN(project_tests)
