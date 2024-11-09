7135405f55e63f834e727c61716e4276f4536bd9
Sergey Bauman <tqqqqt@users.noreply.github.com>
2024-11-09 15:20:07 +0700

    some change, need more window to complete mode choise

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSettings settings("tqqqqt","calculator");
+    if(!settings.contains("calc/mode")) settings.setValue("calc/mode",1);
+    int curent_mode=settings.value("calc/mode",1).toInt();
     MainWindow w;
-    w.show();
+    if(curent_mode==1) w.show();
 
     return a.exec();
 }
