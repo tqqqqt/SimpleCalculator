b50392e32dc5aaf0ecfd8d9f6e45f2300d5cf226
Sergey Bauman <tqqqqt@users.noreply.github.com>
2024-12-06 17:31:08 +0700

    create interface for all objects

---
+++ Calculator/source/programobject.h
@@ -0,0 +1,20 @@
+#ifndef PROGRAMOBJECT_H
+#define PROGRAMOBJECT_H
+
+#include <iostream>
+#include <string>
+
+class ProgramObject{
+public:
+    virtual ~ProgramObject(){ }
+
+    virtual std::string toString()=0;
+    virtual size_t getLength()=0;
+
+    virtual void addNum(char)=0;
+    virtual void deleteLastSymbol()=0;
+    virtual void setFullNum(std::string)=0;
+    virtual void clear()=0;
+};
+
+#endif // PROGRAMOBJECT_H
