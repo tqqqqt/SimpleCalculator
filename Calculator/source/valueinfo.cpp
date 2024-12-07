8c1478d89258faf1c374e75eb6dbb398e758ed37
Sergey Bauman <tqqqqt@users.noreply.github.com>
2024-12-07 16:50:02 +0700

    rewrite functions and fields, need fex speed and data

--- Calculator/source/valueinfo.cpp
+++ Calculator/source/valueinfo.cpp
@@ -1,34 +1,100 @@
 #include "valueinfo.h"
 
+ValueInfo::ValueInfo()
+{
+    createArea();
+    createLength();
+    createTemperature();
+    createVolume();
+    createMass();
+    createData();
+    createSpeed();
+    createTime();
+}
+
+void ValueInfo::createArea(){
+    mains.push_back("Площадь");
+    seconds.push_back({"Акры (ac)","Ары (a)","Гектары (ha)","Квадратные сантиметры (cm^2)","Квадратные футы (ft^2)","Квадратные дюймы (in^2)","Квадратные метры (m^2)"});
+    std::vector<double> data={4046.8564224,100,10000,0.0001,0.09290304,0.00064516,1};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createLength(){
+    mains.push_back("Длина");
+    seconds.push_back({"Миллиметры (mm)","Сантиметры (cm)","Метры (m)","Километры (km)","Дюймы (in)","Футы (ft)","Ярды (yd)","Мили (mi)","Морские мили (NM)","Мили (mil)"});
+    std::vector<double> data={0.001,0.01,1,1000,0.0254,0.3048,0.9144,1609.344,1852,0.0000254};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createTemperature(){
+    mains.push_back("Температура");
+    seconds.push_back({"Градус Цельсия (C)","Градус Фарингейта (F)","Градус Кельвина (K)"});
+}
+
+void ValueInfo::createVolume(){
+    mains.push_back("Объем");
+    seconds.push_back({"Английские галлоны (gal)","Американские галоны (gal)","Литры (l)","Миллилитры (ml)","Кубические сантиметры (cm^3)","Кубические метры (m^3)","Кубические дюймы (in^3)","Кубические футы (ft^3)"});
+    std::vector<double> data={4.54609,3.785411784,1,0.001,0.001,1000,0.016387064,28.316846592};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createMass(){
+    mains.push_back("Масса");
+    seconds.push_back({"Тонны (t)","Английский тонны (t)","Американские тонны (t)","Фунты (lb)","Унции (oz)","Килограммы (kg)","Граммы (g)"});
+    std::vector<double> data={1000,1016.0469088,907.18474,0.45359237,0.0283495231,1,0.001};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createData(){
+    mains.push_back("Данные");
+    seconds.push_back({"Биты (bit)","Байты (B)","Килобайты (KB)","Мегабайты (MB)","Гигабайты (GB)","Терабайты (TB)"});
+    std::vector<double> data={8388608,1048576,1024,1,0.0009765625,0.00000095367431640625};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createSpeed(){
+    mains.push_back("Скорость");
+    seconds.push_back({"Метры в секунду (m/s)","Метры в час (m/h)","Километры в секунду (km/s)","Километры в час (km/h)","Дюймы в секунду (in/s)","Дюймы в час (in/h)","Футы в секунду (ft/s)","Футы в час (ft/h)","Мили в секунду (mi/s)","Мили в час (mi/h)","Узлы (kn)"});
+    std::vector<double> data={1,3600,0.001,3.6,39.3700787402,141732.28346457,3.280839895,11811.023622047,0.0006213712,2.2369362921,1.9438444924};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
+void ValueInfo::createTime(){
+    mains.push_back("Время");
+    seconds.push_back({"Миллисекунды (ms)","Секунды (s)","Минуты (min)","Часы (h)","Дни (d)","Недели (wk)"});
+    std::vector<double> data={0.001,1.0,60.0,3600.0,86400.0,604800.0};
+    for(size_t i=0;i<data.size();i++) info[seconds.back()[i]]=data[i];
+}
+
 std::vector<std::string> ValueInfo::getMain(){
     std::vector<std::string> result;
-    for(auto x:info) result.push_back(x.first);
+    for(auto x:mains) result.push_back(x);
     return result;
 }
 
 std::vector<std::string> ValueInfo::getSecond(std::string _main){
     std::vector<std::string> result;
-    for(auto x:info[_main]) result.push_back(x.first);
+    size_t index=0;
+    if(_main==mains[AREA_INDEX]) index=AREA_INDEX;
+    else if(_main==mains[LENGTH_INDEX]) index=LENGTH_INDEX;
+    else if(_main==mains[TEMPERATURE_INDEX]) index=TEMPERATURE_INDEX;
+    else if(_main==mains[VOLUME_INDEX]) index=VOLUME_INDEX;
+    else if(_main==mains[MASS_INDEX]) index=MASS_INDEX;
+    else if(_main==mains[DATA_INDEX]) index=DATA_INDEX;
+    else if(_main==mains[SPEED_INDEX]) index=SPEED_INDEX;
+    else if(_main==mains[TIME_INDEX]) index=TIME_INDEX;
+    for(auto x:seconds[index]) result.push_back(x);
     return result;
 }
 
-std::string ValueInfo::getMullNum(std::string _main, std::string _left, std::string _right, std::string _value, int _mode){
-    if(info.find(_main)==info.end()) return "error";
-    if(info[_main].find(_left)==info[_main].end()) return "error";
-    if(info[_main][_left].find(_right)==info[_main][_left].end()) return "error";
-    std::string mul_value=info[_main][_left][_right];
-    int up_rating=rating_info[_left], bottom_rating=rating_info[_right];
-    if(mul_value.find('.')!=std::string::npos) mul_value[mul_value.find('.')]=',';
+std::string ValueInfo::getMullNum(std::string _main, std::string _left, std::string _right, std::string _value){
     std::string result="";
-    if(_main!="Температура"){
-        if(_mode==1 && up_rating>=bottom_rating) result=MathDiv(_value,mul_value,10);
-        else if(_mode==2 && bottom_rating>=up_rating) result=MathDiv(_value,mul_value,10);
-        else result=MathMul(_value,mul_value);
+    if(_main!=mains[TEMPERATURE_INDEX]){
+        std::string mul_value=std::to_string(info[_left]/info[_right]);
+        if(mul_value.find('.')!=std::string::npos) mul_value[mul_value.find('.')]=',';
+        result=MathMul(_value,mul_value);
     }
-    else{
-        if(_mode==1) result=getTemperature(_left,_right,_value);
-        else if(_mode==2) result=getTemperature(_right,_left,_value);
-    }
+    else result=getTemperature(_left,_right,_value);
     return result;
 }
 
@@ -42,61 +108,20 @@ std::string ValueInfo::getTemperature(std::string _from, std::string _to, std::s
         if(_to=="Градус Фарингейта (F)") return _value;
         else if(_to=="Градус Цельсия (C)"){
             std::string temp=MathNeg(_value,"32");
-            if(temp[0]=='-') return '-'+MathMul(temp.substr(1),MathDiv("5","9",10));
-            return MathMul(temp,MathDiv("5","9",10));
+            return MathDiv(MathMul(temp,"5"),"9",10);
         }
         else if(_to=="Градус Кельвина (K)"){
-            std::string temp=MathNeg(_value,"32");
-            if(temp[0]=='-') return '-'+MathSum(MathMul(temp.substr(1),MathDiv("5","9",10)),"273,15");
-            return MathSum(MathMul(temp.substr(1),MathDiv("5","9",10)),"273,15");
+            std::string temp=MathSum(_value,"459,67");
+            return MathDiv(MathMul(temp,"5"),"9",10);
         }
     }
     else if(_from=="Градус Кельвина (K)"){
         if(_to=="Градус Кельвина (K)") return _value;
         else if(_to=="Градус Фарингейта (F)"){
             std::string temp=MathNeg(_value,"273,15");
-            if(temp[0]=='-') return '-'+MathSum(MathMul(temp,MathDiv("9","5",10)),"32");
             return MathSum(MathMul(temp,MathDiv("9","5",10)),"32");
         }
         else if(_to=="Градус Цельсия (C)") return MathNeg(_value,"273,15");
     }
     return "error";
-}
-
-ValueInfo::ValueInfo()
-{
-    std::vector<std::string> mains{"Площадь","Длина","Температура","Объем","Масса","Данные","Скорость","Время"};
-
-    std::vector<std::vector<std::string>> second{{"Акры (ac)","Ары (a)","Гектары (ha)","Квадратные сантиметры (cm^2)","Квадратные футы (ft^2)","Квадратные дюймы (in^2)","Квадратные метры (m^2)"},
-                                                {"Миллиметры (mm)","Сантиметры (cm)","Метры (m)","Километры (km)","Дюймы (in)","Футы (ft)","Ярды (yd)","Мили (mi)","Морские мили (NM)","Мили (mil)"},
-                                                {"Градус Цельсия (C)","Градус Фарингейта (F)","Градус Кельвина (K)"},
-                                                {"Английские галлоны (gal)","Американские галоны (gal)","Литры (l)","Миллилитры (ml)","Кубические сантиметры (cm^3)","Кубические метры (m^3)","Кубические дюймы (in^3)","Кубические футы (ft^3)"},
-                                                {"Тонны (t)","Английский тонны (t)","Американские тонны (t)","Фунты (lb)","Унции (oz)","Килограммы (kg)","Граммы (g)"},
-                                                {"Биты (bit)","Байты (B)","Килобайты (KB)","Мегабайты (MB)","Гигабайты (GB)","Терабайты (TB)"},
-                                                {"Метры в секунду (m/s)","Метры в час (m/h)","Километры в секунду (km/s)","Километры в час (km/h)","Дюймы в секунду (in/s)","Дюймы в час (in/h)","Футы в секунду (ft/s)","Футы в час (ft/h)","Мили в секунду (mi/s)","Мили в час (mi/h)","Узлы (kn)"},
-                                                {"Миллисекунды (ms)","Секунды (s)","Минуты (min)","Часы (h)","Дни (d)","Недели (wk)"}};
-
-    std::vector<std::vector<double>> value{{4046.8564224,100,10000,0.0001,0.09290304,0.00064516,1},
-                                          {0.001,0.01,1,1000,0.0254,0.3048,0.9144,1609.344,1852,0.0000254},
-                                          {1,1,1,1,1,1,1,1,1},
-                                          {4.54609,3.785411784,1,0.001,0.001,1000,0.016387064,28.316846592},
-                                          {1000,1016.0469088,907.18474,0.45359237,0.0283495231,1,0.001},
-                                          {8388608,1048576,1024,1,0.0009765625,0.00000095367431640625},
-                                          {1,3600,0.001,3.6,39.3700787402,141732.28346457,3.280839895,11811.023622047,0.0006213712,2.2369362921,1.9438444924},
-                                          {604800000,604800,10080,168,7,1}};
-    for(size_t i=0;i<mains.size();i++){
-        if(i==2) continue;
-        for(size_t j=0;j<second[i].size();j++){
-            rating_info[second[i][j]]=static_cast<int>(j);
-            for(size_t k=0;k<second[i].size();k++){
-                info[mains[i]][second[i][j]][second[i][k]]=std::to_string(static_cast<long double>((value[i][j]/value[i][k])));
-            }
-        }
-    }
-    for(size_t i=0, ii=0;i<second[2].size();i++){
-        for(size_t j=0;j<second[2].size();j++){
-            info[mains[2]][second[2][i]][second[2][j]]=std::to_string(value[2][ii]);
-            ii++;
-        }
-    }
 }
--- Calculator/source/valueinfo.h
+++ Calculator/source/valueinfo.h
@@ -3,23 +3,46 @@
 
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 #include "./numbermath.h"
+
+#define AREA_INDEX 0
+#define LENGTH_INDEX 1
+#define TEMPERATURE_INDEX 2
+#define VOLUME_INDEX 3
+#define MASS_INDEX 4
+#define DATA_INDEX 5
+#define SPEED_INDEX 6
+#define TIME_INDEX 7
 
 class ValueInfo
 {
 private:
-    std::unordered_map</*main*/std::string,std::unordered_map</*left*/std::string,std::unordered_map</*right*/std::string,std::string>>> info;
-    std::unordered_map</*from*/std::string,std::unordered_map<std::string,std::string>> temperature;
-    std::unordered_map<std::string,int> rating_info;
+    std::vector<std::string> mains;
+    std::vector<std::vector<std::string>> seconds;
+    std::unordered_map<std::string,double> info;
+
+    //std::unordered_map</*main*/std::string,std::unordered_map</*left*/std::string,std::unordered_map</*right*/std::string,std::string>>> info;
+    //std::unordered_map</*from*/std::string,std::unordered_map<std::string,std::string>> temperature;
+    //std::unordered_map<std::string,int> rating_info;
 
     std::string getTemperature(std::string, std::string, std::string);
+
+    void createArea();
+    void createLength();
+    void createTemperature();
+    void createVolume();
+    void createMass();
+    void createData();
+    void createSpeed();
+    void createTime();
 
 public:
     ValueInfo();
     std::vector<std::string> getMain();
     std::vector<std::string> getSecond(std::string);
-    std::string getMullNum(std::string, std::string, std::string, std::string, int);
+    std::string getMullNum(std::string, std::string, std::string, std::string);
 };
 
 #endif // VALUEINFO_H
