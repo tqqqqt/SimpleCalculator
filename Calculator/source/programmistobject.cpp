cf59acff4366d6d3d68e486cb9894a41eb24c8ea
Sergey Bauman <tqqqqt@users.noreply.github.com>
2024-12-17 16:27:23 +0700

    added convert to systems

--- Calculator/source/programmistobject.cpp
+++ Calculator/source/programmistobject.cpp
@@ -23,12 +23,26 @@ std::string ProgrammistObject::toString8(){
     return text_8;
 }
 
-std::string ProgrammistObject::toString(){
+std::string ProgrammistObject::toString10(){
     return text_10;
 }
 
 std::string ProgrammistObject::toString16(){
     return text_16;
+}
+
+std::string ProgrammistObject::toString(){
+    switch(curent_system){
+    case 2:
+        return text_2;
+    case 8:
+        return text_8;
+    case 10:
+        return text_10;
+    case 16:
+        return text_16;
+    }
+    return "";
 }
 
 size_t ProgrammistObject::getLength(){
@@ -48,18 +62,85 @@ size_t ProgrammistObject::getLength(){
 void ProgrammistObject::addNum(char _num){
     switch(curent_system){
     case 2:
-        //
+        if(_num!='0' && _num!='1') throw std::invalid_argument("incorect num to 2 system");
+        text_2+=_num;
+        length_2++;
+        convertTo10(2);
+        convertTo(8);
+        convertTo(2);
         break;
     case 8:
-        //
+        if(!(_num>='0' && _num<='7')) throw std::invalid_argument("incorect num to 8 system");
+        text_8+=_num;
+        length_8++;
+        convertTo10(8);
+        convertTo(2);
+        convertTo(16);
         break;
     case 10:
-        //
+        if(!(_num>='0' && _num<='9')) throw std::invalid_argument("incorect num to 10 system");
+        text_10+=_num;
+        length_10++;
+        convertTo(2);
+        convertTo(8);
+        convertTo(16);
         break;
     case 16:
-        //
+        if(!(_num>='0' && _num<='9') && !(_num>='A' && _num<='F')) throw std::invalid_argument("incorect num to 16 system");
+        text_16+=_num;
+        length_16++;
+        convertTo10(16);
+        convertTo(2);
+        convertTo(8);
         break;
     }
+}
+
+void ProgrammistObject::convertTo10(int _system){
+    if(_system!=2 && _system!=8 && _system!=16) return;
+    std::string result="0", num="", temp="", system=std::to_string(_system), pow_num="0";
+    if(_system==2) num=text_2;
+    else if(_system==8) num=text_8;
+    else num=text_16;
+    std::vector<std::string> nums_16={"10","11","12","13","14","15"};
+    for(int i=num.length()-1;i>=0;i--){
+        if(num[i]>='A') temp=nums_16[num[i]-'A'];
+        else temp=num[i];
+        result=MathSum(result,MathMul(temp,MathPow(system,pow_num)));
+        pow_num=MathSum(pow_num,"1");
+    }
+    text_10=result;
+}
+
+void ProgrammistObject::convertTo(int _system){
+    if(_system!=2 && _system!=8 && _system!=16) return;
+    std::string result="", num=text_10, system=std::to_string(_system), mul_num="", neg_num="", temp="";
+    while(MaxNumber(num,system)!=1){
+        mul_num="";
+        neg_num="";
+        temp=MathDiv(num,system,1);
+        for(size_t i=0;i<temp.length();i++){
+            if(temp[i]==',') break;
+            mul_num+=temp[i];
+        }
+        neg_num=MathNeg(num,MathMul(system,mul_num));
+        if(MaxNumber(neg_num,"10")<=0) neg_num=numToSymbol(neg_num);
+        result=neg_num+result;
+        num=mul_num;
+    }
+    result=num+result;
+    if(_system==2) text_2=result;
+    else if(_system==8) text_8=result;
+    else text_16=result;
+}
+
+std::string ProgrammistObject::numToSymbol(std::string _num){
+    if(_num=="10") return "A";
+    if(_num=="11") return "B";
+    if(_num=="12") return "C";
+    if(_num=="13") return "D";
+    if(_num=="14") return "E";
+    return "F";
 }
 
 void ProgrammistObject::deleteLastSymbol(){
--- Calculator/source/programmistobject.h
+++ Calculator/source/programmistobject.h
@@ -4,15 +4,21 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <vector>
 
 #include "programobject.h"
+#include "numbermath.h"
 
 class ProgrammistObject : public ProgramObject
 {
 private:
-    std::string text_2, text_8, text_10, text_16, curent_text;
+    std::string text_2, text_8, text_10, text_16;
     size_t length_2, length_8, length_10, length_16;
     int curent_system, count_dot, count_null;
+
+    void convertTo10(int);
+    void convertTo(int);
+    std::string numToSymbol(std::string);
 
 public:
     ProgrammistObject();
@@ -20,8 +26,9 @@ public:
 
     std::string toString2();
     std::string toString8();
-    std::string toString();
+    std::string toString10();
     std::string toString16();
+    std::string toString();
     size_t getLength();
 
     void addNum(char);
