f55f9c4b26c745865c475ebadadfedb2a6eca771
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-04 18:51:56 +0700

    added new tests without code

--- Test_NumberMath/tst_numbermath.cpp
+++ Test_NumberMath/tst_numbermath.cpp
@@ -13,16 +13,51 @@ public:
 private slots:
     void test_maxNum_data();
     void test_maxNum();
+
     void test_findMultiplier_data();
     void test_findMultiplier();
+
     void test_mathSum_data();
     void test_mathSum();
+
     void test_mathNeg_data();
     void test_mathNeg();
+
     void test_mathMul_data();
     void test_mathMul();
+
     void test_mathDiv_data();
     void test_mathDiv();
+
+    void test_mathPow_data();
+    void test_mathPow();
+
+    void test_mathSin_data();
+    void test_mathSin();
+
+    void test_mathCos_data();
+    void test_mathCos();
+
+    void test_mathTng_data();
+    void test_mathTng();
+
+    void test_mathCtng_data();
+    void test_mathCtng();
+
+    void test_mathFactorial_data();
+    void test_mathFactorial();
+
+    void test_mathMod_data();
+    void test_mathMod();
+
+    void test_mathModule_data();
+    void test_mathModule();
+
+    void test_mathRoundUp_data();
+    void test_mathRoundUp();
+
+    void test_mathRoundDown_data();
+    void test_mathRoundDown();
 };
 
 numbermath::numbermath(){ }
@@ -45,6 +80,10 @@ void numbermath::test_maxNum_data(){
     QTest::newRow("test_9")<<"-2,000001"<<"-2,000001"<<0;
     QTest::newRow("test_10")<<"0,01"<<"0,010"<<0;
     QTest::newRow("test_11")<<"5,99"<<"5,98"<<-1;
+    QTest::newRow("test_12")<<"2,001"<<"2,0010000000001"<<1;
+    QTest::newRow("test_13")<<"-432"<<"-432"<<0;
+    QTest::newRow("test_14")<<"-12"<<"-13"<<-1;
+    QTest::newRow("test_15")<<"2,000000001"<<"2,000000002"<<1;
 }
 
 void numbermath::test_maxNum(){
@@ -52,7 +91,9 @@ void numbermath::test_maxNum(){
     QFETCH(QString,secondNum);
     QFETCH(int,result);
 
-    QCOMPARE(MaxNumber(firstNum.toStdString(),secondNum.toStdString()),result);
+    int curent_result=MaxNumber(firstNum.toStdString(),secondNum.toStdString());
+
+    QCOMPARE(curent_result,result);
 }
 
 void numbermath::test_findMultiplier_data(){
@@ -75,7 +116,9 @@ void numbermath::test_findMultiplier(){
     QFETCH(QString,secondNum);
     QFETCH(int,result);
 
-    QCOMPARE(FindMultiplier(firstNum.toStdString(),secondNum.toStdString()),result);
+    int curent_result=FindMultiplier(firstNum.toStdString(),secondNum.toStdString());
+
+    QCOMPARE(curent_result,result);
 }
 
 void numbermath::test_mathSum_data(){
@@ -103,6 +146,13 @@ void numbermath::test_mathSum_data(){
     QTest::newRow("test_18")<<"4,99"<<"0,01"<<"5,00";
     QTest::newRow("test_19")<<"123456789,2468"<<"987654321,13579"<<"1111111110,38259";
     QTest::newRow("test_20")<<"123456789,987654321"<<"987654321,123456789"<<"1111111111,111111110";
+    QTest::newRow("test_21")<<"1,999"<<"0,001"<<"2,000";
+    QTest::newRow("test_22")<<"23,456"<<"2,441"<<"25,897";
+    QTest::newRow("test_23")<<"2,199199"<<"2,011011"<<"4,210210";
+    QTest::newRow("test_24")<<"2"<<"-45"<<"-43";
+    QTest::newRow("test_25")<<"0,446"<<"-2,1"<<"-1,654";
+    QTest::newRow("test_26")<<"-21,009"<<"0,00078"<<"-21,00822";
+    QTest::newRow("test_27")<<"-13,3009"<<"-14,8007"<<"-28,1016";
 }
 
 void numbermath::test_mathSum(){
@@ -110,7 +160,9 @@ void numbermath::test_mathSum(){
     QFETCH(QString,secondNum);
     QFETCH(QString,result);
 
-    QCOMPARE(MathSum(firstNum.toStdString(),secondNum.toStdString()),result.toStdString());
+    std::string curent_result=MathSum(firstNum.toStdString(),secondNum.toStdString());
+
+    QCOMPARE(curent_result,result.toStdString());
 }
 
 void numbermath::test_mathNeg_data(){
@@ -140,6 +192,12 @@ void numbermath::test_mathNeg_data(){
     QTest::newRow("test_20")<<"2,00001"<<"3,364"<<"-1,36399";
     QTest::newRow("test_21")<<"123456789,123456789"<<"987654321,987654321"<<"-864197532,864197532";
     QTest::newRow("test_22")<<"987654321,123456789"<<"123456789,987654321"<<"864197531,135802468";
+    QTest::newRow("test_23")<<"1"<<"0,0099"<<"0,9901";
+    QTest::newRow("test_24")<<"2"<<"0,00990099"<<"1,99009901";
+    QTest::newRow("test_25")<<"-123,001"<<"2,009"<<"-125,010";
+    QTest::newRow("test_26")<<"45,778"<<"-5,222"<<"51,000";
+    QTest::newRow("test_27")<<"-82,0009"<<"-17,9991"<<"-64,0018";
+    QTest::newRow("test_28")<<"-2,009"<<"7,991"<<"-10,000";
 }
 
 void numbermath::test_mathNeg(){
@@ -147,7 +205,9 @@ void numbermath::test_mathNeg(){
     QFETCH(QString,secondNum);
     QFETCH(QString,result);
 
-    QCOMPARE(MathNeg(firstNum.toStdString(),secondNum.toStdString()),result.toStdString());
+    std::string curent_result=MathNeg(firstNum.toStdString(),secondNum.toStdString());
+
+    QCOMPARE(curent_result,result.toStdString());
 }
 
 void numbermath::test_mathMul_data(){
@@ -175,6 +235,10 @@ void numbermath::test_mathMul_data(){
     QTest::newRow("test_18")<<"23,25"<<"24,26"<<"564,0450";
     QTest::newRow("test_19")<<"123456789,2468"<<"123456789,13579"<<"15241578797423853,937022972";
     QTest::newRow("test_20")<<"123456789987654321,123456789"<<"987654321123456789,987654321"<<"121932632103337905905959456328608443,662094193112635269";
+    QTest::newRow("test_21")<<"-23,23"<<"1119"<<"-25994,37";
+    QTest::newRow("test_22")<<"221,12"<<"-17"<<"-3759,04";
+    QTest::newRow("test_23")<<"-234,21"<<"-13,14"<<"3077,5194";
+    QTest::newRow("test_24")<<"-0,001"<<"0,0000001"<<"-0,0000000001";
 }
 
 void numbermath::test_mathMul(){
@@ -182,7 +246,9 @@ void numbermath::test_mathMul(){
     QFETCH(QString,secondNum);
     QFETCH(QString,result);
 
-    QCOMPARE(MathMul(firstNum.toStdString(),secondNum.toStdString()),result.toStdString());
+    std::string curent_result=MathMul(firstNum.toStdString(),secondNum.toStdString());
+
+    QCOMPARE(curent_result,result.toStdString());
 }
 
 void numbermath::test_mathDiv_data(){
@@ -210,6 +276,9 @@ void numbermath::test_mathDiv_data(){
     QTest::newRow("test_18")<<"1"<<"10"<<"0,1";
     QTest::newRow("test_19")<<"123456789"<<"345"<<"357845,7652173913";
     QTest::newRow("test_20")<<"1"<<"2345"<<"0,0004264392";
+    QTest::newRow("test_21")<<"-324,14"<<"14,15"<<"-22,9074204947";
+    QTest::newRow("test_22")<<"21,12"<<"-6,42"<<"-3,2897196262";
+    QTest::newRow("test_23")<<"-47,786"<<"-34,623"<<"1,3801808047";
 }
 
 void numbermath::test_mathDiv(){
@@ -217,12 +286,95 @@ void numbermath::test_mathDiv(){
     QFETCH(QString,secondNum);
     QFETCH(QString,result);
 
+    std::string curent_result="";
+
     try{
-        QCOMPARE(MathDiv(firstNum.toStdString(),secondNum.toStdString(),10),result.toStdString());
+        curent_result=MathDiv(firstNum.toStdString(),secondNum.toStdString(),10);
+        QCOMPARE(curent_result,result.toStdString());
     }
     catch(std::exception &exp){
         QCOMPARE(exp.what(),result.toStdString());
     }
+}
+
+void numbermath::test_mathPow_data(){
+
+}
+
+void numbermath::test_mathPow(){
+
+}
+
+void numbermath::test_mathSin_data(){
+
+}
+
+void numbermath::test_mathSin(){
+
+}
+
+void numbermath::test_mathCos_data(){
+
+}
+
+void numbermath::test_mathCos(){
+
+}
+
+void numbermath::test_mathTng_data(){
+
+}
+
+void numbermath::test_mathTng(){
+
+}
+
+void numbermath::test_mathCtng_data(){
+
+}
+
+void numbermath::test_mathCtng(){
+
+}
+
+void numbermath::test_mathFactorial_data(){
+
+}
+
+void numbermath::test_mathFactorial(){
+
+}
+
+void numbermath::test_mathMod_data(){
+
+}
+
+void numbermath::test_mathMod(){
+
+}
+
+void numbermath::test_mathModule_data(){
+
+}
+
+void numbermath::test_mathModule(){
+
+}
+
+void numbermath::test_mathRoundUp_data(){
+
+}
+
+void numbermath::test_mathRoundUp(){
+
+}
+
+void numbermath::test_mathRoundDown_data(){
+
+}
+
+void numbermath::test_mathRoundDown(){
+
 }
 
 QTEST_APPLESS_MAIN(numbermath)
