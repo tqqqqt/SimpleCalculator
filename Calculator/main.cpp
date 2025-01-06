9e7773ff7f0ac2d08e20c2715cc59431a0b13cd4
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-06 17:00:36 +0700

    added new window to changer

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -4,24 +4,28 @@
 #include "mainwindow.h"
 #include "Value/valuewindow.h"
 #include "Programmist/programmistwindow.h"
+#include "Graphics/GraphicsInfo/graphicsinfowindow.h"
 
 class WindowChanger: public QObject{
 private:
     MainWindow* calculator_window;
     ValueWindow* value_window;
     ProgrammistWindow* programmist_window;
+    GraphicsInfoWindow* graphics_info_window;
 
 public:
-    void setWindows(MainWindow* _calculator_window, ValueWindow* _value_window, ProgrammistWindow* _programmist_window){
+    void setWindows(MainWindow* _calculator_window, ValueWindow* _value_window, ProgrammistWindow* _programmist_window, GraphicsInfoWindow* _graphics_info_window){
         calculator_window=_calculator_window;
         value_window=_value_window;
         programmist_window=_programmist_window;
+        graphics_info_window=_graphics_info_window;
     }
 
     void curentWindow(int curent_mode){
         if(curent_mode==1) calculator_window->show();
         else if(curent_mode==2) value_window->show();
         else if(curent_mode==3) programmist_window->show();
+        else if(curent_mode==4) graphics_info_window->show();
     }
 };
 
@@ -37,11 +41,13 @@ int main(int argc, char *argv[])
     MainWindow calculator_window;
     ValueWindow value_window;
     ProgrammistWindow programmist_window;
-    window_changer.setWindows(&calculator_window,&value_window,&programmist_window);
+    GraphicsInfoWindow graphics_window;
+    window_changer.setWindows(&calculator_window,&value_window,&programmist_window,&graphics_window);
 
     QObject::connect(&calculator_window,&MainWindow::changeWindow,&window_changer,&WindowChanger::curentWindow);
     QObject::connect(&value_window,&ValueWindow::changeWindow,&window_changer,&WindowChanger::curentWindow);
     QObject::connect(&programmist_window,&ProgrammistWindow::changeWindow,&window_changer,&WindowChanger::curentWindow);
+    QObject::connect(&graphics_window,&GraphicsInfoWindow::changeWindow,&window_changer,&WindowChanger::curentWindow);
 
     window_changer.curentWindow(curent_mode);
 
