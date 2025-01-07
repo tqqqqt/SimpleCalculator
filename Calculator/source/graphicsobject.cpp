67a53607dcc5162e5fc14555327e7300d7332c44
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-07 16:01:46 +0700

    added class for graphics objects

---
+++ Calculator/source/graphicsobject.cpp
@@ -0,0 +1,6 @@
+#include "graphicsobject.h"
+
+GraphicsObject::GraphicsObject()
+{
+
+}
---
+++ Calculator/source/graphicsobject.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICSOBJECT_H
+#define GRAPHICSOBJECT_H
+
+#include "calculatorobject.h"
+
+class GraphicsObject : public CalculatorObject
+{
+public:
+    GraphicsObject();
+};
+
+#endif // GRAPHICSOBJECT_H
