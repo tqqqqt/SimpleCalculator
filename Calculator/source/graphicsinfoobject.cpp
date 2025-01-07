432b07a817da1727e91fcc3d69c1a71f905ccd23
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-07 17:57:13 +0700

    added variables, functions to save and get info

--- Calculator/source/graphicsinfoobject.cpp
+++ Calculator/source/graphicsinfoobject.cpp
@@ -2,5 +2,23 @@
 
 GraphicsInfoObject::GraphicsInfoObject()
 {
+    display_text="";
+}
 
+void GraphicsInfoObject::setDisplayText(std::string _text){
+    display_text=_text;
+}
+
+std::string GraphicsInfoObject::getDisplayText(){
+    std::string result="y = "+display_text;
+    return result;
+}
+
+void GraphicsInfoObject::setPolishEntry(std::vector<CalculatorObject> _polish_entry){
+    polish_entry=_polish_entry;
+}
+
+std::vector<CalculatorObject> GraphicsInfoObject::getPolishEntry(){
+    std::vector<CalculatorObject> result=polish_entry;
+    return result;
 }
--- Calculator/source/graphicsinfoobject.h
+++ Calculator/source/graphicsinfoobject.h
@@ -1,11 +1,25 @@
 #ifndef GRAPHICSINFOOBJECT_H
 #define GRAPHICSINFOOBJECT_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "calculatorobject.h"
 
 class GraphicsInfoObject
 {
+private:
+    std::string display_text;
+    std::vector<CalculatorObject> polish_entry;
+
 public:
     GraphicsInfoObject();
+
+    void setDisplayText(std::string);
+    std::string getDisplayText();
+    void setPolishEntry(std::vector<CalculatorObject>);
+    std::vector<CalculatorObject> getPolishEntry();
 };
 
 #endif // GRAPHICSINFOOBJECT_H
