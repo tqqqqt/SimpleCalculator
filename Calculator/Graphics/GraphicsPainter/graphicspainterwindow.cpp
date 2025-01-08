835351a75ecf1652ceb7dbc7cf7c5108529ee173
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-08 17:40:13 +0700

    added functions to paint ords and points, signals to update graphics

--- Calculator/Graphics/GraphicsPainter/graphicspainterwindow.cpp
+++ Calculator/Graphics/GraphicsPainter/graphicspainterwindow.cpp
@@ -8,6 +8,29 @@ GraphicsPainterWindow::GraphicsPainterWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+GraphicsPainterWindow::GraphicsPainterWindow(QVector<GraphicsInfoObject> *_arr, QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::GraphicsPainterWindow)
+{
+    ui->setupUi(this);
+
+    curent_scale=1;
+    info_objects=_arr;
+    paintMainOrd();
+    paintPoints();
+
+    ui->label->setPixmap(curent_ord);
+
+    // pluss and minus buttons
+    this->connect(ui->pushButton_pluss,SIGNAL(clicked()),this,SLOT(addScale()));
+    this->connect(ui->pushButton_minus,SIGNAL(clicked()),this,SLOT(minusScale()));
+
+    // signals
+    this->connect(this,SIGNAL(needPaintOrd()),this,SLOT(paintMainOrd()));
+    this->connect(this,SIGNAL(needPaintPoints()),this,SLOT(paintPoints()));
+    this->connect(this,SIGNAL(needUpdatePicture()),this,SLOT(updatePicture()));
+}
+
 GraphicsPainterWindow::~GraphicsPainterWindow()
 {
     delete ui;
@@ -20,4 +43,79 @@ void GraphicsPainterWindow::needClose(){
 void GraphicsPainterWindow::closeEvent(QCloseEvent *event){
     emit closeWindow();
     event->accept();
+}
+
+void GraphicsPainterWindow::paintMainOrd(){
+    int window_height=height(),buttons_height=ui->frame->height()+20;
+    int height=window_height-buttons_height, width=QWidget::width()-8;
+
+    QPixmap pixmap(width,height);
+    pixmap.fill(Qt::white);
+    QPainter painter(&pixmap);
+    painter.setPen(QPen(Qt::black,1));
+
+    // X ord
+    painter.drawLine(0,height/2,width,height/2);
+    painter.drawLine(width,height/2,width-10,height/2-10);
+    painter.drawLine(width,height/2,width-10,height/2+10);
+
+    // Y ord
+    painter.drawLine(width/2,0,width/2,height);
+    painter.drawLine(width/2,0,width/2-10,10);
+    painter.drawLine(width/2,0,width/2+10,10);
+
+    curent_ord=pixmap;
+}
+
+void GraphicsPainterWindow::paintPoints(){
+    QPainter painter(&curent_ord);
+    painter.setPen(QPen(Qt::black,1));
+    int points_space=30, width=curent_ord.width(), height=curent_ord.height();
+    // points on X ord
+    for(int i=points_space;i<width/2;i+=points_space){
+        // right points
+        painter.drawLine(width/2+i,height/2-5,width/2+i,height/2+5);
+        painter.drawText(width/2+i-4,height/2+16,QString::fromStdString(std::to_string((i/points_space)*curent_scale)));
+        //left points
+        painter.drawLine(width/2-i,height/2-5,width/2-i,height/2+5);
+        painter.drawText(width/2-i-4,height/2+16,QString::fromStdString(std::to_string(((i/points_space)*curent_scale)*-1)));
+    }
+
+    // point on Y ord
+    for(int i=points_space;i<height/2;i+=points_space){
+        // upper points
+        painter.drawLine(width/2-5,height/2-i,width/2+5,height/2-i);
+        painter.drawText(width/2-17,height/2-i+4,QString::fromStdString(std::to_string((i/points_space)*curent_scale)));
+        //bottom points
+        painter.drawLine(width/2-5,height/2+i,width/2+5,height/2+i);
+        painter.drawText(width/2-17,height/2+i+4,QString::fromStdString(std::to_string(((i/points_space)*curent_scale)*-1)));
+    }
+}
+
+void GraphicsPainterWindow::updatePicture(){
+    emit needPaintOrd();
+    emit needPaintPoints();
+
+    ui->label->setPixmap(curent_ord);
+}
+
+void GraphicsPainterWindow::addScale(){
+    if(curent_scale>=20) return;
+    curent_scale+=1;
+
+    emit needUpdatePicture();
+}
+
+void GraphicsPainterWindow::minusScale(){
+    if(curent_scale<=1) return;
+    curent_scale-=1;
+
+    emit needUpdatePicture();
+}
+
+void GraphicsPainterWindow::paintGraphics(){
+    emit needUpdatePicture();
+
+    if(info_objects->size()==0) return;
+
 }
--- Calculator/Graphics/GraphicsPainter/graphicspainterwindow.h
+++ Calculator/Graphics/GraphicsPainter/graphicspainterwindow.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 #include <QCloseEvent>
+#include <QPixmap>
+#include <QPainter>
+
+#include "./../../source/graphicsinfoobject.h"
+#include "./../../source/calculatormath.h"
 
 namespace Ui {
 class GraphicsPainterWindow;
@@ -14,19 +19,34 @@ class GraphicsPainterWindow : public QMainWindow
 
 public:
     explicit GraphicsPainterWindow(QWidget *parent = nullptr);
+    GraphicsPainterWindow(QVector<GraphicsInfoObject> *arr, QWidget *parent=nullptr);
     ~GraphicsPainterWindow();
 
 private:
     Ui::GraphicsPainterWindow *ui;
+    QVector<GraphicsInfoObject> *info_objects;
+    QPixmap curent_ord;
+    int curent_scale;
 
 private:
     void closeEvent(QCloseEvent*);
 
+public slots:
+    void paintGraphics();
+
 private slots:
     void needClose();
+    void paintMainOrd();
+    void paintPoints();
+    void addScale();
+    void minusScale();
+    void updatePicture();
 
 signals:
     void closeWindow();
+    void needPaintOrd();
+    void needPaintPoints();
+    void needUpdatePicture();
 };
 
 #endif // GRAPHICSPAINTERWINDOW_H
