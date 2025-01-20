e139b391922e20a3f5dfb8d453f0bb59e3a4393b
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-01-20 17:15:49 +0700

    added function to check curent num and delete no need symbols

--- Calculator/source/valueobject.cpp
+++ Calculator/source/valueobject.cpp
@@ -6,6 +6,7 @@ ValueObject::ValueObject()
     length=0;
     count_dot=1;
     count_null=1;
+    check_num_complete=false;
 }
 
 ValueObject::~ValueObject(){ }
@@ -27,6 +28,8 @@ void ValueObject::addNum(char _num){
     if(length==0 && _num==',') return;
     if(length==1 && count_null==0 && count_dot==1 && _num=='0') return;
 
+    check_num_complete=false;
+
     // change first null to num
     if(length==1 && text[0]=='0' && (_num>='1' && _num<='9')){
         text[0]=_num;
@@ -45,6 +48,8 @@ void ValueObject::addNum(char _num){
 // delete last symbol in text
 void ValueObject::deleteLastSymbol(){
     if(length==0) return;
+
+    check_num_complete=false;
 
     if(text.back()==',') count_dot++;
     if(length==1 && count_dot==1 && text.back()=='0') count_null++;
@@ -67,9 +72,52 @@ void ValueObject::setFullNum(std::string _num){
         if(find_dot>1) throw std::invalid_argument("incorect num");
     }
 
+    check_num_complete=false;
     text=_num;
     length=text.length();
     if(_num.find(',')!=std::string::npos) count_dot=0;
+}
+
+void ValueObject::checkNum(){
+    if(check_num_complete==true) return;
+
+    // calculate length to save work
+    length=text.length();
+    if(length==0) return;
+
+    // delete no need first null
+    while(length>0 && text[0]=='0'){
+        text.erase(0,1);
+        length-=1;
+    }
+
+    if(length==0 || (length>0 && text[0]==',')){
+        text='0'+text;
+        length+=1;
+    }
+
+    // check dot part
+    size_t dot_position=text.find(',');
+    if(dot_position==std::string::npos){
+        check_num_complete=true;
+        return;
+    }
+
+    // check count symbols not equal null
+    int count_no_null=0;
+    for(size_t i=dot_position+1;i<length;i++){
+        if(text[i]!='0') count_no_null++;
+    }
+
+    if(count_no_null!=0){
+        check_num_complete=true;
+        return;
+    }
+
+    // delete dot part cause don't have symbols > 0
+    text.erase(dot_position,length-dot_position);
+    length=text.length();
+    check_num_complete=true;
 }
 
 // clear all states in object
@@ -78,4 +126,5 @@ void ValueObject::clear(){
     length=0;
     count_dot=1;
     count_null=1;
+    check_num_complete=false;
 }
